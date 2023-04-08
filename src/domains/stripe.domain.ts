7e081bcde808edb56667133315ceccbeb1b5e204
minami0601 <minami.love.bb@icloud.com>
2023-04-08 13:29:36 +0900

    友達追加URLを修正

--- src/domains/stripe.domain.ts
+++ src/domains/stripe.domain.ts
@@ -24,8 +24,8 @@ export const getStripeCheckoutURL = async (props: CheckoutURLProps): Promise<{ u
       ],
       payment_method_types: ['card'],
       mode,
-      success_url: 'https://lin.ee/F2H8dsW',
-      cancel_url: 'https://lin.ee/F2H8dsW'
+      success_url: 'https://lin.ee/yGQsVIA',
+      cancel_url: 'https://lin.ee/yGQsVIA'
     })
 
     if (url === null) {
